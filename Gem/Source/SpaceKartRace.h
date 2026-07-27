7768cb2440ab76e1657f266b2a0dab592cec12cc
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-07-27 21:13:13 +0200

    Add countdown and original item system state

--- Gem/Source/SpaceKartRace.h
+++ Gem/Source/SpaceKartRace.h
@@ -12,6 +12,22 @@ namespace AzFramework
 
 namespace SpaceKartLegends
 {
+    enum class RacePhase : AZ::u8
+    {
+        Countdown,
+        Racing,
+        Finished
+    };
+
+    enum class ItemType : AZ::u8
+    {
+        None,
+        CometBoost,
+        PlasmaShield,
+        GravityMine,
+        PhotonPulse
+    };
+
     struct CircuitDefinition
     {
         const char* m_name;
@@ -34,8 +50,14 @@ namespace SpaceKartLegends
         float m_boostTime = 0.0f;
         float m_driftCharge = 0.0f;
         float m_airTime = 0.0f;
+        float m_shieldTime = 0.0f;
+        float m_slowTime = 0.0f;
+        float m_spinTime = 0.0f;
+        float m_aiItemDecisionTime = 0.0f;
         int m_lap = 1;
         int m_place = 1;
+        int m_nextPickupIndex = 0;
+        ItemType m_item = ItemType::None;
         bool m_finished = false;
     };
 
@@ -56,6 +78,7 @@ namespace SpaceKartLegends
         void SetBraking(bool braking);
         void SetDrifting(bool drifting);
         void UseBoost();
+        void UseItem();
         void RecoverPlayer();
         void SelectNextCircuit();
 
@@ -67,6 +90,9 @@ namespace SpaceKartLegends
         const DriverState& GetPlayer() const;
         const CircuitDefinition& GetCircuit() const;
         int GetCircuitIndex() const;
+        RacePhase GetRacePhase() const;
+        float GetCountdownTime() const;
+        static const char* GetItemName(ItemType item);
 
         void DrawWorld(AzFramework::DebugDisplayRequests& debugDisplay) const;
         void DrawHud(AzFramework::DebugDisplayRequests& debugDisplay) const;
@@ -74,14 +100,24 @@ namespace SpaceKartLegends
     private:
         void UpdatePlayer(float deltaTime);
         void UpdateAi(float deltaTime);
+        void UpdateDriverTimers(DriverState& driver, float deltaTime);
         void UpdateRanking();
+        void CheckItemPickup(DriverState& driver, int driverIndex, float previousProgress);
+        ItemType SelectPickupItem(const DriverState& driver, int driverIndex) const;
+        void UseDriverItem(int driverIndex);
+        int FindTargetAhead(int driverIndex) const;
+        int FindTargetBehind(int driverIndex) const;
+        void ApplyHit(int targetIndex, float slowTime, float spinTime, float lateralImpulse);
+
         void DrawTrack(AzFramework::DebugDisplayRequests& debugDisplay) const;
+        void DrawItemGates(AzFramework::DebugDisplayRequests& debugDisplay) const;
         void DrawKart(AzFramework::DebugDisplayRequests& debugDisplay, const DriverState& driver, int driverIndex) const;
         void DrawSpaceEnvironment(AzFramework::DebugDisplayRequests& debugDisplay) const;
 
         AZ::Vector3 GetCenterlinePosition(float progress) const;
         static float WrapProgress(float progress);
         static float RaceScore(const DriverState& driver);
+        static float ForwardRaceDistance(const DriverState& from, const DriverState& to);
 
         AZStd::array<CircuitDefinition, CircuitCount> m_circuits;
         AZStd::array<DriverState, DriverCount> m_drivers;
@@ -91,5 +127,7 @@ namespace SpaceKartLegends
         bool m_braking = false;
         bool m_drifting = false;
         float m_elapsedTime = 0.0f;
+        float m_countdownTime = 3.5f;
+        RacePhase m_racePhase = RacePhase::Countdown;
     };
 }
